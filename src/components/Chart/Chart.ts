a17d84b8aaf01db7de4d93b034d29a5f8c5a005e
Sean2108 <sean2108@gmail.com>
2020-04-26 19:37:38 +0100

    make chart interactive

--- src/components/Chart/Chart.ts
+++ src/components/Chart/Chart.ts
@@ -1,6 +1,13 @@
 import * as d3 from "d3";
 import Vue from "vue";
-import { CaseCountRaw, CaseCount } from "@/types";
+import { CaseCountRaw, CaseCount, DataTypes } from "@/types";
+
+const LINE_MAPPING = [
+  { type: DataTypes.CONFIRMED, text: "Confirmed cases", colour: "steelblue" },
+  { type: DataTypes.DEATHS, text: "Deaths", colour: "red" },
+  { type: DataTypes.RECOVERIES, text: "Recoveries", colour: "lightgreen" },
+  { type: DataTypes.ACTIVE, text: "Active cases", colour: "orange" },
+];
 
 export default Vue.extend({
   mounted() {
@@ -13,72 +20,61 @@ export default Vue.extend({
     height: Number,
     legendX: Number,
     legendY: Number,
-    marginHorizontal: Number
+    marginHorizontal: Number,
   },
   watch: {
     data: function(newVal) {
       d3.selectAll("svg").remove();
       this.drawGraph(newVal);
-    }
+    },
   },
   methods: {
-    drawLegend(svg: any) {
+    onMouseEnter(svg: any, colour: string) {
+      svg.select(`.chartlines#line-${colour}`).style("stroke-width", "3");
+      svg
+        .select(`.legendcircles#circle-${colour}`)
+        .attr("r", 8)
+        .style("stroke", "black");
+      svg
+        .selectAll(`.chartlines:not(#line-${colour})`)
+        .style("stroke-width", "0.5");
+    },
+    onMouseLeave(svg: any) {
+      svg.selectAll(".chartlines").style("stroke-width", "1.5");
+      svg
+        .selectAll(".legendcircles")
+        .attr("r", 6)
+        .style("stroke", "transparent");
+    },
+    drawLegend(svg: any, text: string, colour: string, legendYOffset: number) {
       svg
         .append("circle")
         .attr("cx", this.legendX)
-        .attr("cy", this.legendY)
+        .attr("cy", this.legendY + legendYOffset)
         .attr("r", 6)
-        .style("fill", "steelblue");
-      svg
-        .append("circle")
-        .attr("cx", this.legendX)
-        .attr("cy", this.legendY + 20)
-        .attr("r", 6)
-        .style("fill", "red");
-      svg
-        .append("circle")
-        .attr("cx", this.legendX)
-        .attr("cy", this.legendY + 40)
-        .attr("r", 6)
-        .style("fill", "lightgreen");
-      svg
-        .append("circle")
-        .attr("cx", this.legendX)
-        .attr("cy", this.legendY + 60)
-        .attr("r", 6)
-        .style("fill", "orange");
+        .style("fill", colour)
+        .attr("class", "legendcircles")
+        .attr("id", `circle-${colour}`)
+        .on("mouseenter", () => this.onMouseEnter(svg, colour))
+        .on("mouseleave", () => this.onMouseLeave(svg));
+
       svg
         .append("text")
         .attr("x", this.legendX + 10)
-        .attr("y", this.legendY)
-        .text("Confirmed cases")
+        .attr("y", this.legendY + legendYOffset)
+        .text(text)
         .style("font-size", "10px")
         .style("fill", "#fff")
-        .attr("alignment-baseline", "middle");
-      svg
-        .append("text")
-        .attr("x", this.legendX + 10)
-        .attr("y", this.legendY + 20)
-        .text("Deaths")
-        .style("font-size", "10px")
-        .style("fill", "#fff")
-        .attr("alignment-baseline", "middle");
-      svg
-        .append("text")
-        .attr("x", this.legendX + 10)
-        .attr("y", this.legendY + 40)
-        .text("Recoveries")
-        .style("font-size", "10px")
-        .style("fill", "#fff")
-        .attr("alignment-baseline", "middle");
-      svg
-        .append("text")
-        .attr("x", this.legendX + 10)
-        .attr("y", this.legendY + 60)
-        .text("Active cases")
-        .style("font-size", "10px")
-        .style("fill", "#fff")
-        .attr("alignment-baseline", "middle");
+        .attr("alignment-baseline", "middle")
+        .on("mouseenter", () => this.onMouseEnter(svg, colour))
+        .on("mouseleave", () => this.onMouseLeave(svg));
+    },
+    drawLegendSection(svg: any) {
+      let yOffset = 0;
+      LINE_MAPPING.forEach(({ text, colour }) => {
+        this.drawLegend(svg, text, colour, yOffset);
+        yOffset += 20;
+      });
     },
     drawLine(
       svg: any,
@@ -93,6 +89,8 @@ export default Vue.extend({
         .attr("fill", "none")
         .attr("stroke", lineColour)
         .attr("stroke-width", 1.5)
+        .attr("class", "chartlines")
+        .attr("id", `line-${lineColour}`)
         .attr(
           "d",
           d3
@@ -101,7 +99,10 @@ export default Vue.extend({
               return x(d.date as Date);
             })
             .y(getYFunc)
-        );
+            .curve(d3.curveMonotoneX)
+        )
+        .on("mouseenter", () => this.onMouseEnter(svg, lineColour))
+        .on("mouseleave", () => this.onMouseLeave(svg));
     },
     drawGraph(input: Array<CaseCountRaw>) {
       const data: Array<CaseCount> = input.map(
@@ -110,14 +111,14 @@ export default Vue.extend({
           confirmed,
           deaths,
           recovered,
-          active: Math.max(confirmed - deaths - recovered, 0)
+          active: Math.max(confirmed - deaths - recovered, 0),
         })
       );
       const margin = {
           top: 10,
           right: this.marginHorizontal,
           bottom: 20,
-          left: this.marginHorizontal
+          left: this.marginHorizontal,
         },
         width = this.width - margin.left - margin.right,
         height = this.height - margin.top - margin.bottom;
@@ -148,24 +149,17 @@ export default Vue.extend({
           0,
           d3.max(data, function(d) {
             return +d.confirmed;
-          }) as number
+          }) as number,
         ])
         .range([height, 0]);
       svg.append("g").call(d3.axisLeft(y));
-      this.drawLine(svg, data, x, "steelblue", (d: CaseCount) => {
-        return y(d.confirmed);
-      });
-      this.drawLine(svg, data, x, "red", (d: CaseCount) => {
-        return y(d.deaths);
-      });
-      this.drawLine(svg, data, x, "lightgreen", (d: CaseCount) => {
-        return y(d.recovered);
-      });
-      this.drawLine(svg, data, x, "orange", (d: CaseCount) => {
-        return y(d.active);
-      });
+      LINE_MAPPING.forEach(({ type, colour }) =>
+        this.drawLine(svg, data, x, colour, (d: CaseCount) => {
+          return y(d[type]);
+        })
+      );
 
-      this.drawLegend(svg);
-    }
-  }
+      this.drawLegendSection(svg);
+    },
+  },
 });
